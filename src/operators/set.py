0f1429b263be2a49f8dc75ea53d90bc32e0e9fb0
moses <moses@booknbloom.com>
2017-12-10 00:21:25 +0200

    add comments at set, string

--- src/operators/set.py
+++ src/operators/set.py
@@ -1,26 +1,78 @@
 def SET_EQUALS(*expressions):
+    """
+    Compares two or more arrays and returns true if they have the same distinct elements and false otherwise.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/setEquals/
+    for more details
+    :param expressions: The arrays (expressions) which will be compared
+    :return: Aggregation operator
+    """
     return {'$setEquals': list(expressions)}
 
 
 def SET_INTERSECTION(*expressions):
+    """
+    Takes two or more arrays and returns an array that contains the elements that appear in every input array.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/setIntersection/
+    for more details
+    :param expressions: The arrays (expressions)
+    :return: Aggregation operator
+    """
     return {'$setIntersection': list(expressions)}
 
 
 def SET_UNION(*expressions):
+    """
+    Takes two or more arrays and returns an array containing the elements that appear in any input array.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/setUnion/
+    for more details
+    :param expressions: The arrays (expressions)
+    :return: Aggregation operator
+    """
     return {'$setUnion': list(expressions)}
 
 
 def SET_DIFFERENCE(*expressions):
+    """
+    Takes two sets and returns an array containing the elements that only exist in the first set.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/setDifference/
+    for more details
+    :param expressions: The arrays (expressions)
+    :return: Aggregation operator
+    """
     return {'$setDifference': list(expressions)}
 
 
 def SET_IS_SUBSET(*expressions):
+    """
+    Takes two arrays and returns true when the first array is a subset of the second,
+    including when the first array equals the second array, and false otherwise.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/setIsSubset/
+    for more details
+    :param expressions: The arrays (expressions)
+    :return: Aggregation operator
+    """
     return {'$setIsSubset': list(expressions)}
 
 
 def ANY_ELEMENT_TRUE(*expressions):
+    """
+    Evaluates an array as a set and returns true if any of the elements are true and false otherwise.
+    An empty array returns false.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/anyElementTrue/
+    for more details
+    :param expressions: The arrays (expressions)
+    :return: Aggregation operator
+    """
     return {'$anyElementTrue': list(expressions)}
 
 
 def ALL_ELEMENTS_TRUE(*expressions):
+    """
+    Evaluates an array as a set and returns true if no element in the array is false. Otherwise, returns false.
+    An empty array returns true.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/allElementsTrue/
+    for more details
+    :param expressions: The arrays (expressions)
+    :return: Aggregation operator
+    """
     return {'$allElementsTrue': list(expressions)}
--- src/operators/string.py
+++ src/operators/string.py
@@ -1,44 +1,141 @@
 def CONCAT(*expressions):
+    """
+    Concatenates strings and returns the concatenated string.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/concat/
+    for more details
+    :param expressions: The list of strings (variables or expressions)
+    :return: Aggregation operator
+    """
     return {'$concat': list(expressions)}
 
 
 def SPLIT(expression, delimiter):
+    """
+    Divides a string into an array of substrings based on a delimiter.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/split/
+    for more details
+    :param expression: The string or expression of the string
+    :return: Aggregation operator
+    """
     return {'$split': [expression, delimiter]}
 
 
 def STR_LEN_BYTES(expression):
+    """
+    Returns the number of UTF-8 encoded bytes in the specified string.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/strLenBytes/
+    for more details
+    :param expression: The string or expression of the string
+    :return: Aggregation operator
+    """
     return {'$strLenBytes': expression}
 
 
 def STR_LEN_CP(expression):
+    """
+    Returns the number of UTF-8 code points in the specified string.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/strLenCP/
+    for more details
+    :param expression: The string or expression of the string
+    :return: Aggregation operator
+    """
     return {'$strLenCP': expression}
 
 
 def STR_CASE_CMP(x, y):
+    """
+    Performs case-insensitive comparison of two strings. Returns
+        1 if first string is “greater than” the second string.
+        0 if the two strings are equal.
+        -1 if the first string is “less than” the second string.
+
+    https://docs.mongodb.com/manual/reference/operator/aggregation/strcasecmp/
+    for more details
+    :param x: The first string or expression of string
+    :param y: The second string or expression of string
+    :return: Aggregation operator
+    """
     return {'$strcasecmp': [x, y]}
 
 
 def SUB_STR(string, start, length):
+    """
+    Returns a substring of a string, starting at a specified index position
+    and including the specified number of characters.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/substr/
+    for more details
+    :param string: The string or expression of string
+    :param start: Indicates the starting point of the substring.
+    :param length: Can be any valid expression as long as it resolves to a non-negative integer or number
+    :return: Aggregation operator
+    """
     return {'$substr': [string, start, length]}
 
 
 def SUB_STR_BYTES(string, index, length):
+    """
+    Returns the substring of a string.
+    The substring starts with the character at the specified UTF-8 byte index (zero-based) in the string
+    and continues for the number of bytes specified.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/substrBytes/
+    for more details
+    :param string: The string or expression of string
+    :param index: Indicates the starting point of the substring.
+    :param length: Can be any valid expression as long as it resolves to a non-negative integer or number
+    :return: Aggregation operator
+    """
     return {'$substrBytes': [string, index, length]}
 
 
 def SUB_STR_CP(string, index, count):
+    """
+    Returns the substring of a string.
+    The substring starts with the character at the specified UTF-8 code point (CP) index (zero-based)
+    in the string for the number of code points specified.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/substrCP/
+    for more details
+    :param string: The string or expression of string
+    :param index: Indicates the starting point of the substring.
+    :param length: Can be any valid expression as long as it resolves to a non-negative integer or number
+    :return: Aggregation operator
+    """
     return {'$substrCP': [string, index, count]}
 
 
 def TO_LOWER(expression):
+    """
+    Converts a string to lowercase, returning the result.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/toLower/
+    for more details
+    :param expression: The string or expression of string
+    :return: Aggregation operator
+    """
     return {'$toLower': expression}
 
 
 def TO_UPPER(expression):
+    """
+    Converts a string to uppercase, returning the result.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/toUpper/
+    for more details
+    :param expression: The string or expression of string
+    :return: Aggregation operator
+    """
     return {'$toUpper': expression}
 
 
 def INDEX_OF_BYTES(string_expression, substring_expression, start=None, end=None):
+    """
+    Searches a string for an occurence of a substring and returns the UTF-8 byte index (zero-based) of the first occurence.
+    If the substring is not found, returns -1.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/indexOfBytes/
+    for more details
+    :param string_expression: The string or expression of string
+    :param substring_expression: The string or expression of substring
+    :param start: An integral number (or expression) that specifies the starting index position for the search.
+    :param end: An integral number (or expression) that specifies the ending index position for the search.
+    :return: Aggregation operator
+    """
     res = [string_expression, substring_expression]
     if start is not None:
         res.append(start)
@@ -48,6 +145,17 @@ def INDEX_OF_BYTES(string_expression, substring_expression, start=None, end=None
 
 
 def INDEX_OF_CP(string_expression, substring_expression, start=None, end=None):
+    """
+    Searches a string for an occurence of a substring and returns the UTF-8 code point index (zero-based) of the first occurence.
+    If the substring is not found, returns -1.
+    https://docs.mongodb.com/manual/reference/operator/aggregation/indexOfCP/
+    for more details
+    :param string_expression: The string or expression of string
+    :param substring_expression: The string or expression of substring
+    :param start: A number that can be represented as integers (or expression), that specifies the starting index position for the search.
+    :param end: A number that can be represented as integers (or expression), that specifies the ending index position for the search.
+    :return: Aggregation operator
+    """
     res = [string_expression, substring_expression]
     if start is not None:
         res.append(start)
